5d8d8d99f59b00b4c50bbc0402656145177adeb1
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-04-08 09:09:14 -0400

    Initial commit

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,10 @@
+// functions1.rs
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
+
+
+fn call_me(x :&str){
+   println!("{x} is so cool"); 
+}
+fn main() {
+    call_me("Dog");
+}
